73ac7fb1b52cc09150f51dee9e409fbd06721f15
Hamza Omer Blala <hamzaomarblala@gmail.com>
2023-02-24 07:03:42 -0800

    7th commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
 // selectors
 let preLoader = document.querySelector('.preloader')
 let navbar =document.querySelector('.mynavbar')
+let linkNavbar =document.querySelectorAll('.mynavbar .links .link a')
 let barIcon =document.querySelector('.bar-icon')
 let closeIcon =document.querySelector('.close-icon')
 let sideBar = document.querySelector('.sidebar')
 let social =document.querySelector('.social')
 let copy =document.querySelector('.copy')
+
 
 // start preLoader
 window.addEventListener('load',()=>{
@@ -19,8 +21,14 @@ window.addEventListener('load',()=>{
 window.addEventListener('scroll',()=>{
     if(window.scrollY >= '200'){
         navbar.classList.add('nav-scroll')
+        linkNavbar.forEach(el =>{
+            el.classList.add("nav-scrollColor")
+        })
     }else{
         navbar.classList.remove('nav-scroll')
+        linkNavbar.forEach(el =>{
+            el.classList.remove("nav-scrollColor")
+        })
     }
 
     if(window.scrollY>='580'){
