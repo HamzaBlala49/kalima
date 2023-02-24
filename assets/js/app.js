2284abba47809b9f5c66b7e11389a6b8bc8ca5a0
Hamza Omer Blala <hamzaomarblala@gmail.com>
2023-02-24 07:19:42 -0800

    8th commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,11 +2,13 @@
 let preLoader = document.querySelector('.preloader')
 let navbar =document.querySelector('.mynavbar')
 let linkNavbar =document.querySelectorAll('.mynavbar .links .link a')
+let linkSideBar =document.querySelectorAll('.sidebar .links .link a')
 let barIcon =document.querySelector('.bar-icon')
 let closeIcon =document.querySelector('.close-icon')
 let sideBar = document.querySelector('.sidebar')
 let social =document.querySelector('.social')
 let copy =document.querySelector('.copy')
+
 
 
 // start preLoader
@@ -44,6 +46,14 @@ closeIcon.onclick=(_=>{
     sideBar.style.display ='none'
 })
 //end navber
+
+// start sideBar
+linkSideBar.forEach(el =>{
+    el.addEventListener('click', ()=>{
+        sideBar.style.display ='none'
+    })
+})
+//end sideBar
 
 // start copy
 let date = new Date()
